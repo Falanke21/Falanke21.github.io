4a6339dc870392d8019dd24f8c818fce494816c4
Falanke21 <1065314944@qq.com>
2019-08-31 05:50:27 -0400

    add typing effect

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 window.onload = function greet(){
 	var myDate = new Date()
 	var hours = myDate.getHours()
-	var displayTime = String(myDate.getHours()) + ":" + String(myDate.getMinutes())
+	var min = String(myDate.getMinutes())
+	if (min.length == 1) {
+		min = "0" + min;
+	}
+	var displayTime = String(myDate.getHours()) + ":" + min
 	var greeting = document.getElementById("greeting-1")
-	var result = "It's " + displayTime + " now"
-
-	greeting.innerHTML = result
+	var txt = "It's " + displayTime + " now"
 
 	if (hours < 12 && hours > 6) {
-		result = "Good morning from Downtown Toronto"
+		var result = "Good morning from Downtown Toronto"
 	} else if (hours >= 12 && hours < 18) {
-		result = "Good afternoon from Downtown Toronto"
+		var result = "Good afternoon from Downtown Toronto"
 	} else if (hours >= 18 && hours < 22) {
-		result = "Good evening from Downtown Toronto"
+		var result = "Good evening from Downtown Toronto"
 	} else {
-		result = "Good night from Downtown Toronto"
+		var result = "Good night from Downtown Toronto."
 	}
 
-	greeting = document.getElementById("greeting-2")
-	greeting.innerHTML = result
+	var i = 0;
+	setTimeout(typeWriter, 800);
 
+	function typeWriter() {
+	  	if (i < txt.length) {
+		    document.getElementById("greeting-1").innerHTML += txt.charAt(i);
+		    i++;
+			setTimeout(typeWriter, 150);
+	  }
+	}
 
+	var j = 0;
+	setTimeout(typeWriterTwo, 3500);
+	function typeWriterTwo() {
+	  	if (j < result.length) {
+		    document.getElementById("greeting-2").innerHTML += result.charAt(j);
+		    j++;
+			setTimeout(typeWriterTwo, 100);
+	  }
+	}
+
+	setTimeout(displayButton, 7500);
+	function displayButton() {
+		document.getElementById("me-button").style.visibility = "visible"
+	}
 }
