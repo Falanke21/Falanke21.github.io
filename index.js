e3f74afbda6668e81dab78935ef713b49741e653
Falanke21 <1065314944@qq.com>
2019-08-31 01:36:02 -0400

    add button, add black-white style to site

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 window.onload = function greet(){
 	var myDate = new Date()
 	var hours = myDate.getHours()
-	var greeting = document.getElementById("greeting")
-
-	if (hours < 12 && hours > 6) {
-		result = "Good Morning"
-	} else if (hours >= 12 && hours < 18) {
-		result = "Good afternoon"
-	} else if (hours >= 18 && hours < 22) {
-		result = "Good evening"
-	} else {
-		result = "Good night"
-	}
-
+	var displayTime = String(myDate.getHours()) + ":" + String(myDate.getMinutes())
+	var greeting = document.getElementById("greeting-1")
+	var result = "It's " + displayTime + " now"
 
 	greeting.innerHTML = result
+
+	if (hours < 12 && hours > 6) {
+		result = "Good morning from Downtown Toronto"
+	} else if (hours >= 12 && hours < 18) {
+		result = "Good afternoon from Downtown Toronto"
+	} else if (hours >= 18 && hours < 22) {
+		result = "Good evening from Downtown Toronto"
+	} else {
+		result = "Good night from Downtown Toronto"
+	}
+
+	greeting = document.getElementById("greeting-2")
+	greeting.innerHTML = result
+
+
 }
